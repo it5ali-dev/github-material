c8a279ce4fcc65e12f49153666addbe35f93528a
Ali Hassan <alihassan.yskt@gmail.com>
2024-08-15 07:27:19 +0500

    complete LightCode Web Design

--- modernUI-tailwind-css/script.js
+++ modernUI-tailwind-css/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
+// navbar
+const navbar = document.querySelector("header");
+
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("bg-primary-dark");
+    navbar.classList.add("border-b");
+    navbar.classList.add("border-gray");
+  } else {
+    navbar.classList.remove("bg-primary-dark");
+    navbar.classList.remove("border-b");
+    navbar.classList.remove("border-gray");
+  }
+};
+
 // Mobile menu
 const bars = document.querySelector("#bars");
 const menu = document.querySelector("#menu");
