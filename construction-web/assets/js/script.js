6cc264bbe6f853772f966a834160903d2a546cd1
Ali Hassan <alihassan.yskt@gmail.com>
2024-08-28 05:56:35 +0500

    complete hero section along with navbar

---
+++ construction-web/assets/js/script.js
@@ -0,0 +1,42 @@
+// Show menu
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", function () {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+// remove menu Mobile
+const navLink = document.querySelectorAll(".nav_link");
+
+const linkAction = function () {
+  const navMenu = document.getElementById("nav-menu");
+  //   when we click on each nav-link show-menu class will be removed
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((link) => {
+  link.addEventListener("click", linkAction);
+});
+
+// change background header
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", bgHeader);
+bgHeader();
