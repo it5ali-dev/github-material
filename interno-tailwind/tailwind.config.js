797eaec139e806457f26da6b491643d948d7fc10
Ali Hassan <alihassan.yskt@gmail.com>
2024-09-06 18:30:06 +0500

    start interno project using tailwind css

--- interno-tailwind/tailwind.config.js
+++ interno-tailwind/tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./*.html", "./assets/js/*.js"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        default: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "DM Serif Display",
+      secondary: "Jost",
+    },
+    backgroundImage: {
+      hero: "URL(./assets/images/hero/bg.jpg)",
+      grid: "URL(./assets/images/grid.png)",
+    },
+    extend: {
+      colors: {
+        primary: {
+          default: "#292f36",
+          hover: "#343e4a",
+        },
+        secondary: "#4d5053",
+        accent: {
+          default: "#cda274",
+          secondary: "#f4f0ec",
+          hover: "#b88c5d",
+        },
+      },
+    },
   },
   plugins: [],
 };
