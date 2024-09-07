8aec017438b64a0fa36af616e1d88a9fd2760aed
Ali Hassan <alihassan.yskt@gmail.com>
2024-09-07 22:29:46 +0500

    complete web design

--- interno-tailwind/assets/js/script.js
+++ interno-tailwind/assets/js/script.js
@@ -7,3 +7,28 @@ const navMenu = document.getElementById("nav_menu");
 hamburger.addEventListener("click", function () {
   navMenu.classList.toggle("nav-open");
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  lopp: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  //   breakpoint
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    960: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// scroll reveal animation
