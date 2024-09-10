7d522c4447451ce4816d4f94ec4bc9a6aa03e098
Ali Hassan <alihassan.yskt@gmail.com>
2024-09-10 13:30:58 +0500

    complete home page, almost done about page

--- html-css-js-projects/education-web/assets/js/script.js
+++ html-css-js-projects/education-web/assets/js/script.js
@@ -2,6 +2,7 @@
 
 // change nav background color on scroll
 window.addEventListener("scroll", function () {
+  // menuBtn.style.display = "none";
   this.document
     .querySelector("nav")
     .classList.toggle("window-scroll", this.window.scrollY > 0);
@@ -43,3 +44,27 @@ const swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+// hamburger
+const menu = document.querySelector(".nav_menu");
+const lists = document.querySelectorAll(".nav_menu ul li");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+const closeNav = () => {
+  menu.style.display = "none";
+  menuBtn.style.display = "inline-block";
+  closeBtn.style.display = "none";
+};
+
+closeBtn.addEventListener("click", closeNav);
+
+// lists.forEach((list) => {
+//   list.addEventListener("click", closeNav);
+// });
