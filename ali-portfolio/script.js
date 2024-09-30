cfe9bee736470065aba3aa7470bfe1bbf9a92080
Ali Hassan <alihassan.yskt@gmail.com>
2024-09-30 21:17:47 +0500

    complete portfolio web

--- ali-portfolio/script.js
+++ ali-portfolio/script.js
@@ -65,11 +65,63 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
 
   breakpoints: {
-    600: {
+    678: {
       slidesPerView: 2,
     },
     1024: {
       slidesPerView: 3,
     },
   },
+});
+
+// Nav Toggle
+const navMenu = document.querySelector(".nav_menu");
+const navMenuList = document.querySelectorAll(".nav_menu li");
+const navOpen = document.querySelector(".nav_open");
+const navClose = document.querySelector(".nav_close");
+
+const openNav = () => {
+  navMenu.style.right = "0";
+  navOpen.style.display = "none";
+  navClose.style.display = "inline-block";
+};
+
+const closeNav = () => {
+  navMenu.style.right = "-15rem";
+  navOpen.style.display = "inline-block";
+  navClose.style.display = "none";
+};
+
+navMenuList.forEach((item) => {
+  item.addEventListener("click", closeNav);
+});
+
+navOpen.addEventListener("click", openNav);
+navClose.addEventListener("click", closeNav);
+
+// theme toggle
+const themeBtn = document.querySelector(".nav_theme");
+
+themeBtn.addEventListener("click", () => {
+  let bodyClass = document.body.className;
+  if (!bodyClass) {
+    bodyClass = "dark";
+    document.body.className = bodyClass;
+    // change toggle icon
+    themeBtn.innerHTML = "<i class='ri-sun-line'></i>";
+    // save theme local storage
+    window.localStorage.setItem("theme", bodyClass);
+  } else {
+    bodyClass = "";
+    document.body.className = bodyClass;
+    // change toggle icon
+    themeBtn.innerHTML = "<i class='ri-moon-fill'></i>";
+    // save theme local storage
+    window.localStorage.setItem("theme", bodyClass);
+  }
+});
+
+// onload theme
+window.addEventListener("load", () => {
+  document.body.className = window.localStorage.getItem("theme");
 });
